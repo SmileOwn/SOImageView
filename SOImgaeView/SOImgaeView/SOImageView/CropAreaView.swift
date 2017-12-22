6c4326b30ff2383d22fa2da7c2c12eb1c374a70a
董家祎 <575801327@qq.com>
2017-12-22 12:05:55 +0800

    绘制四个角

--- SOImgaeView/SOImgaeView/SOImageView/CropAreaView.swift
+++ SOImgaeView/SOImgaeView/SOImageView/CropAreaView.swift
@@ -10,11 +10,82 @@ import UIKit
 
 class CropAreaView: UIView {
 
+    var cornerHeight = 6.0
+   private var beyond = 5.0
+    
+    private var cornerWidth = 40.0
+    
+    var layerWidth = 0.5
+    
+    var topLeft:UIView = UIView()
+    var leftTop:UIView = UIView()
+    var topRight:UIView = UIView()
+    var rightTop:UIView = UIView()
+    var leftBottom:UIView = UIView()
+    var bottomLeft:UIView = UIView()
+    var rightBottom:UIView = UIView()
+    var bottomRight:UIView = UIView()
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.stup()
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        self.stup()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.updateFrame()
+    }
+    
+    func updateFrame() -> Void {
+        
+    topLeft.frame = CGRect(x: -beyond, y: -(cornerHeight-layerWidth), width: cornerWidth, height: cornerHeight)
+        
+    leftTop.frame = CGRect(x: -(beyond), y: 0, width: cornerHeight, height: cornerWidth)
+        
+    topRight.frame = CGRect(x: Double(self.bounds.size.width - CGFloat(cornerWidth)) + beyond+layerWidth, y: -(cornerHeight-layerWidth), width: cornerWidth, height: cornerHeight)
+        
+    rightTop.frame = CGRect(x: Double(self.bounds.size.width)-layerWidth, y: 0, width: cornerHeight, height: cornerWidth)
+        
+     leftBottom.frame = CGRect(x: -beyond, y: Double(self.bounds.size.height)-cornerWidth+beyond, width: cornerHeight, height: cornerWidth)
+        
+    bottomLeft.frame = CGRect(x: -beyond, y: Double(self.bounds.size.height) - layerWidth, width: cornerWidth, height: cornerHeight)
+        
+    rightBottom.frame = CGRect(x: Double(self.bounds.size.width) - layerWidth, y: Double(self.bounds.size.height) - cornerWidth + beyond, width: cornerHeight, height: cornerWidth)
+        
+    bottomRight.frame = CGRect(x:  Double(self.bounds.size.width - CGFloat(cornerWidth)), y: Double(self.bounds.size.height) - layerWidth, width: cornerWidth, height: cornerHeight)
+        
+    }
+    func stup() -> Void {
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = CGFloat(layerWidth)
+        topLeft.backgroundColor = UIColor.white
+        self.addSubview(topLeft)
+        
+        leftTop.backgroundColor = UIColor.white
+        self.addSubview(leftTop)
+        
+        topRight.backgroundColor = UIColor.white
+        self.addSubview(topRight)
+        
+        rightTop.backgroundColor = UIColor.white
+        self.addSubview(rightTop)
+        
+        leftBottom.backgroundColor = UIColor.white
+        self.addSubview(leftBottom)
+        bottomLeft.backgroundColor = UIColor.white
+        self.addSubview(bottomLeft)
+        
+        rightBottom.backgroundColor = UIColor.white
+        self.addSubview(rightBottom)
+        
+        bottomRight.backgroundColor = UIColor.white
+        self.addSubview(bottomRight)
     }
 
 }
--- SOImgaeView/SOImgaeView/SOImageView/SOImageView.swift
+++ SOImgaeView/SOImgaeView/SOImageView/SOImageView.swift
@@ -14,6 +14,11 @@ class SOImageView: UIView {
     var shadeView:UIView      = UIView()
     var croAreaView:CropAreaView = CropAreaView()
     
+    var cro_width = 100.0
+    var cro_height = 100.0
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.stup()
@@ -25,6 +30,7 @@ class SOImageView: UIView {
     }
     func stup() -> Void {
         cropImageView.frame = self.bounds
+        cropImageView.isUserInteractionEnabled = true
         cropImageView.image = UIImage(named: "IMG_3398")
         cropImageView.contentMode = .scaleAspectFit
         self.addSubview(cropImageView)
@@ -33,25 +39,55 @@ class SOImageView: UIView {
         shadeView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
         cropImageView.addSubview(shadeView)
         
-        croAreaView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-    
+        croAreaView.frame = CGRect(x: 0, y: 0, width: cro_width, height: cro_height)
+        
+        
         cropImageView.addSubview(croAreaView)
         croAreaView.center = cropImageView.center
        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panCropAreaView(sender:)))
+        croAreaView.addGestureRecognizer(panGestureRecognizer)
+        
+        self.resetCropArea()
+        
+    }
+    @objc func panCropAreaView(sender:UIPanGestureRecognizer) -> Void {
+      
+        let translation = sender.translation(in: self.cropImageView)
+     
+        var point = CGPoint(x: (sender.view?.center.x)! + translation.x, y: (sender.view?.center.y)! + translation.y)
+        
+        point.y = max((sender.view?.frame.size.height)!/2, point.y)
+        
+        point.y = min(self.cropImageView.frame.size.height - (sender.view?.frame.size.height)! / 2,point.y)
+        
+        point.x = max((sender.view?.frame.size.width)! / 2, point.x)
+        
+        point.x = min(self.cropImageView.frame.size.width - (sender.view?.frame.size.width)! / 2,point.x)
+        
+        
+        sender.view?.center = point
+        
+        
+        sender.setTranslation(CGPoint.zero, in: self.cropImageView)
+        
+        
+        self.resetCropArea()
+        
+        
+        
+    }
+    func resetCropArea() -> Void {
         let path = UIBezierPath(rect: cropImageView.bounds)
-        let clearPath = UIBezierPath(rect: croAreaView.frame).reversing()
+        let clearPath = UIBezierPath(rect:croAreaView.frame).reversing()
+        
         path.append(clearPath)
         var shapeLayer = shadeView.layer.mask as? CAShapeLayer
         if shapeLayer == nil {
             shapeLayer = CAShapeLayer()
-              shadeView.layer.mask = shapeLayer
+            shadeView.layer.mask = shapeLayer
         }
         shapeLayer?.path = path.cgPath
-        
-      
-        
-      
-        
     }
     
     
