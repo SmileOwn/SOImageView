4aa24cdd9f0bfd01dfd2ada37cd6fddf96b1e170
董家祎 <575801327@qq.com>
2018-01-08 15:41:39 +0800

    四个角落

--- SOImgaeView/SOImgaeView/SOImageView/CropAreaView.swift
+++ SOImgaeView/SOImgaeView/SOImageView/CropAreaView.swift
@@ -44,6 +44,14 @@ class CropAreaView: UIView {
     var rightBottom:UIView = UIView()
     var bottomRight:UIView = UIView()
     
+    var cornerLTView = UIView()
+    
+    var cornerRTView = UIView()
+    
+    var cornerLBView = UIView()
+    
+    var cornerRBView = UIView()
+    
     
     
     override init(frame: CGRect) {
@@ -78,6 +86,13 @@ class CropAreaView: UIView {
         
     bottomRight.frame = CGRect(x: Double(self.bounds.size.width - CGFloat(cornerWidth)) + beyond+layerWidth, y: Double(self.bounds.size.height) - layerWidth, width: cornerWidth, height: cornerHeight)
         
+        
+        cornerLTView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        cornerRTView.frame = CGRect(x: self.bounds.size.width-40, y: 0, width: 40, height: 40)
+        
+        cornerLBView.frame = CGRect(x: 0, y: self.bounds.size.height - 40, width: 40, height: 40)
+        cornerRBView.frame = CGRect(x: self.bounds.size.width-40, y: self.bounds.size.width-40, width: 40, height: 40)
+        
     }
     func stup() -> Void {
         self.layer.borderColor = UIColor.white.cgColor
@@ -105,101 +120,75 @@ class CropAreaView: UIView {
         bottomRight.backgroundColor = UIColor.white
         self.addSubview(bottomRight)
       
+        self.addSubview(cornerLBView)
+        self.addSubview(cornerLTView)
+        self.addSubview(cornerRTView)
+        self.addSubview(cornerRBView)
+        
         self.addPanGesture()
         
     }
     
     func addPanGesture() -> Void {
         
-        let pan_tf = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        topLeft.tag = 1001
-        topLeft.addGestureRecognizer(pan_tf)
         
-        let pan_lt = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        leftTop.tag = 1002
-        leftTop.addGestureRecognizer(pan_lt)
+        //左上
+        let pan_tf = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
+        cornerLTView.tag = 1001
+        cornerLTView.addGestureRecognizer(pan_tf)
         
          let pan_tr = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        topRight.tag = 1003
-        topRight.addGestureRecognizer(pan_tr)
+        cornerRTView.tag = 1002
+        cornerRTView.addGestureRecognizer(pan_tr)
       
-        let pan_rt = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        rightTop.tag = 1004
-        rightTop.addGestureRecognizer(pan_rt)
-       
         let pan_lb = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        leftBottom.addGestureRecognizer(pan_lb)
-        let pan_bl = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        bottomLeft.addGestureRecognizer(pan_bl)
+        cornerLBView.tag = 1003
+        cornerLBView.addGestureRecognizer(pan_lb)
         
-         let pan_rb = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        rightBottom.addGestureRecognizer(pan_rb)
         
-           let pan_br = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
-        bottomRight.addGestureRecognizer(pan_br)
+        let pan_rb = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(sender:)))
         
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(pinchGestureAction(sender:)))
+        cornerRBView.addGestureRecognizer(pan_rb)
         
-        self.addGestureRecognizer(pinch)
+        
         
     }
 
-   @objc func pinchGestureAction (sender:UIPinchGestureRecognizer) -> Void {
 
-        if sender.state == .began {
-            self.pichSize = self.frame.size
-            
-        }
-    
-        if sender.state == .changed {
-            self.resetScaleFactor(scale: sender.scale)
-        }
-    
-    }
-    
-    
-    func resetScaleFactor(scale:CGFloat) -> Void {
-        let center = self.center
-       
-        var width = self.pichSize.width * scale
-        var height = self.pichSize.height * scale
-        
-        width = width < minWidth ? minHeight :  (width > minWidth * 2.0 ? minWidth * 2.0 : width)
-        
-        height = height < minHeight ? minHeight : (height > minHeight * 2.0 ? minHeight * 2.0 : height)
-        
-        
-        self.bounds = CGRect(x: 0, y: 0, width: width, height: height)
-        self.center = center
-    
-        self.delegate?.cropFrameChange(scale: 0.0)
-        
-    }
     @objc func panGestureAction(sender:UIPanGestureRecognizer) -> Void {
-        let point = sender.translation(in: self)
+        let offPoint = sender.translation(in: self)
+
+        
         if sender.state == .began {
             self.pichSize = self.frame.size
             self.pichCenter = self.center
           
         }
         if sender.state == .changed {
-            var x  = point.x
-            var y = point.y
+            var x  = offPoint.x
+            var y = offPoint.y
             
-            let centerAvg = (point.x + point.y) / 2
+            let centerAvg = (offPoint.x + offPoint.y) / 2
             
-            if sender.view?.tag == 1001 || sender.view?.tag == 1002 {
-                x = -(point.x)
-                y = -(point.y)
+            if sender.view?.tag == 1001 {
+                x = -(offPoint.x)
+                y = -(offPoint.y)
             }
             
-            if sender.view?.tag == 1003 || sender.view?.tag == 1004{
-
-                y =  -(point.y)
-                x =  -(point.x)
+            if sender.view?.tag == 1002 || sender.view?.tag == 1003{
+                if offPoint.x > 0 && offPoint.y < 0{
+                    x = -(offPoint.x)
+                    y = offPoint.y
+                }
+                
+                if offPoint.x < 0 && offPoint.y > 0{
+                    x = offPoint.x
+                    y = -(offPoint.y)
+                }
                
-              
+               
             }
+
             
             let avg = (x + y) / 2
             var width = self.pichSize.width + avg
@@ -212,8 +201,13 @@ class CropAreaView: UIView {
             
             self.frame.size = CGSize(width: width, height: height)
             
-            self.center = CGPoint(x: self.pichCenter.x + centerAvg/2, y: self.pichCenter.y + centerAvg / 2)
-          
+            if sender.view?.tag == 1003 || sender.view?.tag == 1002 {
+                self.center = CGPoint(x: self.pichCenter.x + offPoint.x/2, y: self.pichCenter.y + offPoint.y/2)
+               
+            }else{
+                
+                self.center = CGPoint(x: self.pichCenter.x + centerAvg/2, y: self.pichCenter.y + centerAvg / 2)
+            }
             
             self.delegate?.cropFrameChange(scale: 0.0)
         }
